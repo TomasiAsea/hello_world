4901f06efe5b309b6098590395285f385b167466
Tomasi <tasea@student.refactory.academy>
2024-04-17 10:27:57 +0300

    hello world with express and java script

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => { 
+    res.send('Homepage! Hello world.');
+  });
+
+  app.get('/about', (req, res) => { // new
+    res.send('About page. Nice.');
+  });
+
+//always last
+app.listen(3000, () => console.log('listening on port 3000'));
